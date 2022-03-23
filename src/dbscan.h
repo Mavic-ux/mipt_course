a60652e70194e2c61adb0e6995a02abb6ff31c30
Aznaur <aliev.ak@phystech.edu>
2022-03-23 18:42:49 -0400

    changed bfs queue

--- src/dbscan.h
+++ src/dbscan.h
@@ -9,7 +9,7 @@ namespace lidar_course {
 
 
 template<typename PointT>
-auto dbscan(typename pcl::PointCloud<PointT>::Ptr input_cloud_ptr,  const float eps = 0.5f, const size_t minPts = 3){
+auto dbscan(typename pcl::PointCloud<PointT>::Ptr input_cloud_ptr,  const float eps = 0.25f, const size_t minPts = 4){
 
     pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
     kdtree.setInputCloud(input_cloud_ptr);
@@ -35,6 +35,11 @@ auto dbscan(typename pcl::PointCloud<PointT>::Ptr input_cloud_ptr,  const float 
             std::vector<int> neighbours;
             std::vector<float> ndists;
 
+            //kdtree.setEpsilon(eps);
+            //kdtree.nearestKSearch(point, 4, neighbours, ndists);
+        
+            //std::sort(ndists.begin(), ndists.end());
+       
             kdtree.radiusSearch(point, eps, neighbours, ndists); //RangeQuery
 
             if (neighbours.size() < minPts){ // Density check
@@ -46,19 +51,19 @@ auto dbscan(typename pcl::PointCloud<PointT>::Ptr input_cloud_ptr,  const float 
 
             labels[i] = cluster_id;
 
-            std::queue<int> bfs_queue;
+            std::vector<int> bfs_queue;
 
             for (const int nbour_id : neighbours){
                 if (nbour_id == i){
                     continue;
                 }
-                bfs_queue.push(nbour_id);
+                bfs_queue.push_back(nbour_id);
             }
 
 
-            while (bfs_queue.size() > 0){
-                int index_q = bfs_queue.front();
-                bfs_queue.pop();
+            while (!bfs_queue.empty()){
+                int index_q = bfs_queue.back();
+                bfs_queue.pop_back();
                 const auto point_q = input_cloud_ptr->points[index_q];
 
                 if (labels[index_q] == NOISE){
@@ -78,12 +83,11 @@ auto dbscan(typename pcl::PointCloud<PointT>::Ptr input_cloud_ptr,  const float 
 
                 if (neighbours.size() >= minPts){ // Density check
                     for (const int nbour_id : neighbours){
-                        bfs_queue.push(nbour_id);
+                        bfs_queue.push_back(nbour_id);
                     }
                 }
             }
         }
-
     }
 
     auto labeled_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZL>>();
